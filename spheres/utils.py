3002096cef743d41cdd28df057ab3b4ed4f28c12
Ali Ghorashi <aligho@mit.edu>
2025-11-14 13:10:47 -0500

    added graphene conductivity to utils.py

--- spheres/utils.py
+++ spheres/utils.py
@@ -1,5 +1,8 @@
 from scipy.special import jv, hankel2, yv, spherical_jn, spherical_yn
-from numpy import sqrt
+from numpy import sqrt, heaviside, pi
+import numpy as np
+
+alpha = 1/137
 
 def hl2(n, x):
     return spherical_jn(n, x) - 1j * spherical_yn(n, x)
@@ -16,7 +19,6 @@ def hl1prime(n, x):
     hn = hl1(n, x)
     hn1 = hl1(n+1, x)
     return n/x * hn - hn1
-
 
 
 # We define g(omega) = i*sigma/(omega*epsilon_0*R) = i*[sigma/(x_0*c*epsilon_0)], where R is the radius of the sphere and sigma is the surface conductivity and x_0=R*(omega/c). 
@@ -59,10 +61,18 @@ def h_r_cpa_denominator(l, x, epsilon, h_omegas=0):
     
     return spherical_jn(l, y) * xhl1_prime - yjl_prime * hl1(l, x) + h_omegas * spherical_jn(l, y) * hl1(l, x)
 
-
-
 def e_r_cpa(l, x, epsilon, g_omegas=0): 
     return e_r_cpa_numerator(l, x, epsilon, g_omegas)/e_r_cpa_denominator(l, x, epsilon, g_omegas)
 
 def h_r_cpa(l, x, epsilon, h_omegas=0): 
     return h_r_cpa_numerator(l, x, epsilon, h_omegas)/h_r_cpa_denominator(l, x, epsilon, h_omegas)
+
+# Graphene-specific data
+
+# Get the graphene conductivity in units of c*epsilon_0
+# We pass in the Fermi energy (in eV), the frequency, by which we mean hbar*omega (in eV), and the scattering rate, hbar/tau in eV
+def graphene_conductivity(epsilon_F, hbar_omega, gamma, include_interband=False):
+    conductivity = 4j*alpha*epsilon_F/(hbar_omega+1j*gamma)
+    if include_interband: 
+        conductivity += alpha*pi*heaviside(hbar_omega-2*epsilon_F, 0.5) + 1j*alpha*np.log(np.abs(2*epsilon_F-hbar_omega)/np.abs(2*epsilon_F+hbar_omega))
+    return conductivity 
